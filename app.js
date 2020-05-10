e7abb5a3abf20cb0865f4b8d7afb62a69288fa1b
Douglas Tofoli <contato@dougrt.dev>
2020-05-10 17:58:58 -0300

    New routes implemented in app.js

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const path = require('path');
+const methodOverride = require('method-override');
 
 const checkListRouter = require('./src/routes/checklist');
+const taskRouter = require('./src/routes/task');
 const rootRouter = require('./src/routes/index');
 
 require('./config/database');
 
 const app = express();
+
+// Middlewares
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method', { methods: ['POST', 'GET'] }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
 
+// Use routes
 app.use('/', rootRouter);
 app.use('/checklists', checkListRouter);
+app.use('/checklists', taskRouter.checklistDepedent);
+app.use('/tasks', taskRouter.taskRouter);
 
-app.listen(3000);
+app.listen(7777);
